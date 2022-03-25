75987e900024cb73a793a60fe1770c3621cb2f93
Ayo crescent <70281852+anayo-hub@users.noreply.github.com>
2022-03-25 12:36:05 +0100

    from first branch

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const food = function(cars = 5){
+    for(let i = 0; i > cars; i++){
+        console.log(cars[i]);
+    }
+}
 let a = 2;
 if(a < 2){
 console.log('i\'m suffering and happy the same time ahh!');
 }else{
     console.log('ewu ofia');
+}
+
+let age = 20;
+    
+    
+
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
 }
